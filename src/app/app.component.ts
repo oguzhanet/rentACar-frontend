6d0b97f456b7efae34c2587a080c3a8c6ba4897f
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-03-12 20:22:20 +0300

    12.03.2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'rentACar';
+  title: string = 'rentACar';
+  user: string = 'Oğuzhan Cengiz';
+  
+
 }
