71a4025e54170a830cfeac2d9db7a0923b2590c6
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-03-15 21:22:33 +0300

    15.03.2021

--- src/app/services/rental.service.ts
+++ src/app/services/rental.service.ts
@@ -7,7 +7,7 @@ import { RentalResponseModel } from '../models/rentalResponseModel';
   providedIn: 'root'
 })
 export class RentalService {
-  apiUrl='https://localhost:44357/api/rentals/getall'
+  apiUrl='https://localhost:44357/api/rentals/getrentaldetails'
 
   constructor(private httpClient:HttpClient) { }
 
